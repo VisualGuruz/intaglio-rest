30bf5abeba2d36e073137f37c599b1d280cfbe86
Codey Whitt <codey.whitt@pearson.com>
2014-06-17 16:51:17 -0600

    Changed the module into a HAPI plugin

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-module.exports = {
-	server: require('./lib/server'),
-	serializers: require('./lib/serializers'),
-	decorator: require('./lib/decorator')
+var pkg = require('./package.json'),
+	server = require('./lib/server');
+
+module.exports = function (orm) {
+	var register = function register (plugin, options, next) {
+		// Setup the server
+		server(orm, plugin);
+
+		next();
+	};
+
+	// Add the attributes to the plugin
+	register.attributes = {pkg: pkg}
+
+	// Return the plugin
+	return register;
 };
